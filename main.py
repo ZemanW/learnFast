fee9ec02180c9570442a881a2b7e8ab965a21d2b
rabeeh <zamanrabeehwork@gmail.com>
2026-01-06 22:57:15 +0530

    pydantic validation

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class post(BaseModel):
+    title:str
+    content:str
+    published: bool = True
+    rating: int = None 
 
 @app.get("/")
 async def root():
     return {"message":"Hello World"}
+
+@app.post("/createposts")
+def create_post(new_post : post):
+    print(new_post.published)
+    return {"new_post":"Okay"}
